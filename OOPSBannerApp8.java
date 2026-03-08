a5a23d4c9281dc3afa13e27052ee527df111c306
anshitaa12 <anshitasingh1217@gmail.com>
2026-03-08 22:25:55 +0530

    Coded UC8 to Print OOPS to Console

---
+++ OOPSBannerApp8.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+public class OOPSBannerApp8 {
+    public static HashMap<Character, String[]> createCharacterMap() {
+        HashMap<Character, String[]> charMap = new HashMap<>();
+        String[] O = {
+            "   ***    ",
+			" **   **  ",
+			"**     ** ",
+			"**     ** ",
+			"**     ** ",
+			"**     ** ",
+			"**     ** ",
+			" **   **  ",
+			"   ***    "
+        };
+
+        String[] P = {
+            "*****   ",
+            "**   ** ",
+            "**    **",
+            "**   ** ",
+            "*****   ",
+            "**      ",
+            "**      ",
+			"**      ",
+			"**      "
+        };
+
+        String[] S = {
+            "    ***** ",
+            "  **      ",
+            " **       ",
+            "  **      ",
+            "    ***   ",
+            "       ** ",
+			"        **",
+			"       ** ",
+            "  *****   "
+        };
+
+        charMap.put('O', O);
+        charMap.put('P', P);
+        charMap.put('S', S);
+        return charMap;
+    }
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+        int patternHeight = 9;
+        for (int line = 0; line < patternHeight; line++) {
+            StringBuilder sb = new StringBuilder();
+            for (char ch : message.toCharArray()) {
+                String[] pattern = charMap.get(ch);
+                if (pattern != null) {
+                    sb.append(pattern[line]).append(" ");
+                }
+            }
+            System.out.println(sb.toString());
+        }
+    }
+    public static void main(String[] args) {
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        String message = "OOPS";
+        displayBanner(message, charMap);
+    }
+}
